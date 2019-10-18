5aa5d4e1e6fa349430e5883d73d30d875c8da552
Adrián <noe.ricoy@hotmail.com>
2019-10-17 22:05:18 -0500

    Se ha agregado el método HandlerUser que nos mandará a la vista con el mismo nombre, que ayudará a las acciones de agregar, eliminar y actualizar usuario

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,37 +12,88 @@ namespace Login.Controllers
         }
 
         [HttpPost]
-        public IActionResult Index(string actions ,string email, string password, string status)
+        public IActionResult Index(string actions ,string email, string password)
         {
             UserCount userCount = new UserCount(email, password);
 
             switch(actions)
             {
-                case "get in":
+                case "Get in":
                     string userStatus = userCount.status == 0 ? "Jefe" : "empleado";
 
                     if (userCount.email != null) ViewData["Success"] = "Todo bien " + userStatus;
                     else ViewData["Danger"] = "No es un usuario valido";
                     break;
-                case "delete":
-                    if (new UserCount().DeleteUserCount(userCount.idUserCount, null))
-                        ViewData["Success"] = "Se han eliminado el usuario";
-                    break;
-                case "update":
-
-                    if (new UserCount().UpdateUserCount(userCount))
-                        ViewData["Success"] = "Se ha actualizado el usuario";
-                    break;
-                case "add":
-                    userCount.email = email;
-                    userCount.password = password;
-                    userCount.status = int.Parse(status);
-
-                    if (new UserCount().AddUserCount(userCount))
-                        ViewData["Success"] = "Se ha agregado un usuario";
-                    break;
             }
 
+            return View();
+        }
+        [HttpGet]
+        public IActionResult HandleUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult HandleUser(string actions, string email, string password, string status)
+        {
+            UserCount user = new UserCount(email, password);
+            switch (actions)
+            {
+                case "Add":
+                    if (!string.IsNullOrEmpty(user.email))
+                    {
+                        ViewData["Success"] = "El usuario ya existe";
+                        return View();
+                    } else
+                    {
+                        UserCount userCount = new UserCount();
+                        userCount.email = email;
+                        userCount.password = password;
+                        userCount.status = int.Parse(status);
+
+                        if(new UserCount().AddUserCount(userCount))
+                        {
+                            ViewData["Success"] = "Se ha agregado el usuario de forma correcta";
+                            return View();
+                        }
+                    }
+                    break;
+                case "Update":
+                    if (string.IsNullOrEmpty(user.email))
+                    {
+                        ViewData["Success"] = "El usuario no existe";
+                        return View();
+                    } else
+                    {
+                        UserCount userCount = new UserCount();
+                        userCount.email = email;
+                        userCount.password = password;
+                        userCount.status = int.Parse(status);
+                        userCount.idUserCount = user.idUserCount;
+
+                        if (new UserCount().UpdateUserCount(userCount))
+                        {
+                            ViewData["Success"] = "Se ha actualzado de forma correcta";
+                            return View();
+                        }
+                    }
+                    break;
+                case "Delete":
+                    if (string.IsNullOrEmpty(user.email))
+                    {
+                        ViewData["Success"] = "El usuario no existe";
+                        return View();
+                    }
+                    else
+                    {
+                        if(new UserCount().DeleteUserCount(user.idUserCount, user.email))
+                        {
+                            ViewData["Success"] = "El usuario ha sido eliminado de forma correcta";
+                            return View();
+                        }
+                    }
+                        break;
+            }
             return View();
         }
 
